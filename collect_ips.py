eff3761c8a45ef56ec43b6f3948e8d2de3e43e9e
Madison <133603530+madison80309@users.noreply.github.com>
2025-05-15 09:59:14 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,7 +4,7 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://www.wetest.vip/page/cloudflare/address_v4.html', 
+urls = ['https://www.itdog.cn/tcping/ips.madison.eu.org:443', 
         'https://ip.164746.xyz']
 
 # 正则表达式用于匹配IP地址
@@ -24,7 +24,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://www.wetest.vip/page/cloudflare/address_v4.html':
+        if url == 'https://www.itdog.cn/tcping/ips.madison.eu.org:443':
             elements = soup.find_all('tr')
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
